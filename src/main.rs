10dd2bdc22d863a06b3b6640c4d04bbb5883feaa
Hyeon Kim <simnalamburt@gmail.com>
2023-05-10 22:42:17 +0900

    Implement a dummy HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::{get, post};
+
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new()
+        .route("/", get(|| async { "" }))
+        .route("/*all", get(|| async { "" }))
+        .route("/", post(|| async { "" }))
+        .route("/*all", post(|| async { "" }));
+
+    axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
