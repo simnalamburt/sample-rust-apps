ce3a41a9bc825e181e19a4734de5215d19191dc8
Hyeon Kim <simnalamburt@gmail.com>
2023-05-11 00:54:27 +0900

    Implement a rate monitor

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
-use std::sync::Arc;
-use std::sync::atomic::{AtomicU64, Ordering};
 use axum::routing::{get, post};
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::thread;
+use std::time::{Duration, Instant};
 
-async fn handler(counter: Arc<AtomicU64>) {
-    let prev = counter.fetch_add(1, Ordering::SeqCst);
-    println!("{prev}");
+static COUNTER: AtomicU64 = AtomicU64::new(0);
+
+async fn handler() {
+    COUNTER.fetch_add(1, Ordering::SeqCst);
 }
 
 #[tokio::main]
 async fn main() {
-    let counter = Arc::new(AtomicU64::new(0));
+    thread::spawn(move || {
+        let mut prev = Instant::now();
+        println!("rate\tcount\telapsed seconds");
+        loop {
+            thread::sleep(Duration::from_millis(250));
 
-    let make_handler = || {
-        let counter = counter.clone();
-        move || handler(counter)
-    };
+            let count = COUNTER.swap(0, Ordering::SeqCst);
+
+            let now = Instant::now();
+            let elapsed = now.duration_since(prev).as_secs_f64();
+            let speed = count as f64 / elapsed;
+            println!("{speed}\t{count}\t{elapsed}");
+
+            prev = now;
+        }
+    });
 
     let app = axum::Router::new()
-        .route("/", get(make_handler()))
-        .route("/*all", get(make_handler()))
-        .route("/", post(make_handler()))
-        .route("/*all", post(make_handler()));
+        .route("/", get(handler))
+        .route("/*all", get(handler))
+        .route("/", post(handler))
+        .route("/*all", post(handler));
 
     axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
         .serve(app.into_make_service())
