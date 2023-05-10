332e6127189460b462a932fd980b5d781c886361
Hyeon Kim <simnalamburt@gmail.com>
2023-05-10 23:08:39 +0900

    Implement a counter

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU64, Ordering};
 use axum::routing::{get, post};
+
+async fn handler(counter: Arc<AtomicU64>) {
+    let prev = counter.fetch_add(1, Ordering::SeqCst);
+    println!("{prev}");
+}
 
 #[tokio::main]
 async fn main() {
+    let counter = Arc::new(AtomicU64::new(0));
+
+    let make_handler = || {
+        let counter = counter.clone();
+        move || handler(counter)
+    };
+
     let app = axum::Router::new()
-        .route("/", get(|| async { "" }))
-        .route("/*all", get(|| async { "" }))
-        .route("/", post(|| async { "" }))
-        .route("/*all", post(|| async { "" }));
+        .route("/", get(make_handler()))
+        .route("/*all", get(make_handler()))
+        .route("/", post(make_handler()))
+        .route("/*all", post(make_handler()));
 
     axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
         .serve(app.into_make_service())
