f4df00b8b3d09e393653a3453493db4e52e00417
Hyeon Kim <simnalamburt@gmail.com>
2025-01-14 18:09:03 +0900

    csv-practice: Simple CSV practice

---
+++ csv-practice/src/main.rs
@@ -0,0 +1,28 @@
+use std::{error::Error, io::stdin};
+
+use csv::Reader;
+use fastnum::{decimal::Context, UD128};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut reader = Reader::from_reader(stdin().lock());
+
+    let mut count = 0;
+    let mut sum = UD128::ZERO;
+    let context = Context::default();
+
+    for row in reader.records() {
+        let cell = &row?[6];
+        let revenue = UD128::from_str(cell, context)?;
+
+        count += 1;
+        sum += revenue;
+    }
+
+    if !sum.is_op_ok() {
+        return Err("Numerical error occurred".into());
+    }
+
+    println!("count: {count}\nsum: {sum}");
+
+    Ok(())
+}
